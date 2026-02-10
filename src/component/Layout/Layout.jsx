f0c52aeb9c12d50e0e5402c97c9fe905d086c20f
Mahmoud Azab <mahmoudazaab034@gmail.com>
2026-02-10 12:04:21 +0200

    update a layout

--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -1,36 +1,12 @@
-// import { Outlet } from "react-router-dom";
-// import Navbar from "../Navbar/Navbar";
-// function Layout() {
-//   return (
-//     <>
-//       <Navbar />
-//       <div style={{ paddingTop: "60px" }}>
-//         <Outlet />
-
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Layout;
-
-// src/component/Layout/Layout.jsx
-
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 
 function Layout() {
-  
-
-  // Apply theme class to the root element
-
-
   return (
     <div className={`min-h-screen`}>
       <Navbar />
       <div className="pt-16">
         {" "}
-        {/* Adjust padding to account for fixed navbar */}
         <Outlet />
       </div>
     </div>
